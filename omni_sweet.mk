d67ba6713475a35908e0772f32ecd2c5cc136418
brucetech627 <66194009+brucetech627@users.noreply.github.com>
2021-06-02 08:23:12 +0530

    fixup

--- omni_sweet.mk
+++ omni_sweet.mk
@@ -21,7 +21,7 @@ DEVICE_PATH := device/xiaomi/sweet
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/cm/config/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/xiaomi/sweet/device.mk)
